2fc5ec22a52f29c5eeed753894612fd22ba7c2fd
Bobby Radford <bobby@rownd.io>
2022-10-24 17:21:41 -0400

    fix: dont reload url in webview

--- android/src/main/java/io/rownd/android/views/HubComposableBottomSheet.kt
+++ android/src/main/java/io/rownd/android/views/HubComposableBottomSheet.kt
@@ -5,9 +5,13 @@ import android.util.Log
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.viewinterop.AndroidViewBinding
 import io.rownd.android.Rownd
 import io.rownd.android.databinding.HubViewLayoutBinding
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 
 enum class HubBottomSheetBundleKeys(val key: String) {
@@ -25,21 +29,33 @@ class HubComposableBottomSheet : ComposableBottomSheetFragment() {
             (bundle?.getSerializable(HubBottomSheetBundleKeys.TargetPage.key)
                 ?: HubPageSelector.Unknown) as HubPageSelector
         val jsFnArgsAsJson = bundle?.getString(JS_FN_OPTIONS)
+        val coroutineScope = rememberCoroutineScope()
 
         Log.d("HubComposableBottomSheet", "jsFnArgsAsJson: $jsFnArgsAsJson")
+
+        val (hasLoadedUrl, setHasLoadedUrl) = remember { mutableStateOf(false) }
 
         val parent = this
         AndroidViewBinding(
             factory = HubViewLayoutBinding::inflate,
             update = {
-            this.hubWebview.dialog = parent
-            val url = Rownd.config.hubLoaderUrl()
-            this.hubWebview.progressBar = this.hubProgressBar
-            this.hubWebview.setIsLoading = setIsLoading
-            this.hubWebview.targetPage = targetPage ?: HubPageSelector.SignIn
-            this.hubWebview.jsFunctionArgsAsJson = jsFnArgsAsJson ?: "{}"
-            this.hubWebview.loadUrl(url)
-        })
+                this.hubWebview.dialog = parent
+                val url = Rownd.config.hubLoaderUrl()
+                this.hubWebview.progressBar = this.hubProgressBar
+                this.hubWebview.setIsLoading = setIsLoading
+                this.hubWebview.targetPage = targetPage ?: HubPageSelector.SignIn
+                this.hubWebview.jsFunctionArgsAsJson = jsFnArgsAsJson ?: "{}"
+                this.hubWebview.animateBottomSheet = {
+                    coroutineScope.launch {
+                        bottomSheetState.animateTo(it)
+                    }
+                }
+                if (!hasLoadedUrl) {
+                    this.hubWebview.loadUrl(url)
+                    setHasLoadedUrl(true)
+                }
+            }
+        )
     }
 
     companion object {
