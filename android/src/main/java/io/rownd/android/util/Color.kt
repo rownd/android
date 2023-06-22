e62849bdd7d2e5d3bdbe690bbe511c3eeb489d61
mfmurray <mfmurray@umich.edu>
2023-06-22 14:26:57 -0400

    first implementation of native material 3 bottom sheet

--- android/src/main/java/io/rownd/android/util/Color.kt
+++ android/src/main/java/io/rownd/android/util/Color.kt
@@ -1,5 +1,12 @@
 package io.rownd.android.util
 
+import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.toColorInt
+
 fun convertRGBtoString(R: Float, G: Float, B: Float): String {
     return "rgba(${(R*255).toInt()} ${(G*255).toInt()} ${(B*255).toInt()})"
+}
+
+fun convertStringToColor(color: String): Color {
+    return Color(color.toColorInt())
 }
---
+++ android/src/main/java/io/rownd/android/util/bottom/sheet/tokens/ShapeTokens.kt
@@ -0,0 +1,44 @@
+package io.rownd.android.util.bottom.sheet.tokens
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+
+
+internal object ShapeTokens {
+    val CornerExtraLarge = RoundedCornerShape(28.0.dp)
+    val CornerExtraLargeTop =
+        RoundedCornerShape(
+            topStart = 28.0.dp,
+            topEnd = 28.0.dp,
+            bottomEnd = 0.0.dp,
+            bottomStart = 0.0.dp
+        )
+    val CornerExtraSmall = RoundedCornerShape(4.0.dp)
+    val CornerExtraSmallTop = RoundedCornerShape(
+        topStart = 4.0.dp,
+        topEnd = 4.0.dp,
+        bottomEnd = 0.0.dp,
+        bottomStart = 0.0.dp
+    )
+    val CornerFull = CircleShape
+    val CornerLarge = RoundedCornerShape(16.0.dp)
+    val CornerLargeEnd =
+        RoundedCornerShape(
+            topStart = 0.0.dp,
+            topEnd = 16.0.dp,
+            bottomEnd = 16.0.dp,
+            bottomStart = 0.0.dp
+        )
+    val CornerLargeTop =
+        RoundedCornerShape(
+            topStart = 16.0.dp,
+            topEnd = 16.0.dp,
+            bottomEnd = 0.0.dp,
+            bottomStart = 0.0.dp
+        )
+    val CornerMedium = RoundedCornerShape(12.0.dp)
+    val CornerNone = RectangleShape
+    val CornerSmall = RoundedCornerShape(8.0.dp)
+}
