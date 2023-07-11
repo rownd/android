6adddcb6a4bf9d89db5cb11b49d9e9f9e1356996
mfmurray <mfmurray@umich.edu>
2023-07-11 12:24:33 -0400

    feat: state subscription to fetchPasskeyRegistration

--- android/src/main/java/io/rownd/android/models/repos/PasskeysRepo.kt
+++ android/src/main/java/io/rownd/android/models/repos/PasskeysRepo.kt
@@ -1,13 +1,10 @@
 package io.rownd.android.models.repos
 
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import android.util.Log
 import io.rownd.android.models.domain.PasskeysState
 import io.rownd.android.models.network.PasskeyRegistrationResponse
 import io.rownd.android.models.network.PasskeysApi
+import kotlinx.coroutines.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -19,11 +16,26 @@ class PasskeysRepo @Inject constructor() {
     @Inject
     lateinit var stateRepo: StateRepo
 
+    private var isLoading: Boolean = false
+
     internal fun get(): PasskeysState {
         return stateRepo.getStore().currentState.passkeys
     }
 
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            stateRepo.getStore().stateAsStateFlow().collect() {
+                if (
+                    it.auth.accessToken != null && !it.passkeys.isInitialized && !isLoading
+                ) {
+                    fetchPasskeyRegistration()
+                }
+            }
+        }
+    }
+
     internal fun fetchPasskeyRegistration(): Deferred<PasskeyRegistrationResponse?> {
+        isLoading = true
         return CoroutineScope(Dispatchers.IO).async {
             val result = passkeysApi.client.fetchRegistration()
                 .onSuccess {
@@ -34,6 +46,7 @@ class PasskeysRepo @Inject constructor() {
                     Log.e("Rownd", "Passkey registration fetch failed! ${it.message}")
                 }
 
+            isLoading = false
             return@async result.getOrNull()
         }
     }
