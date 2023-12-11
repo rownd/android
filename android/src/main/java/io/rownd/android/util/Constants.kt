62aedf759b046ca419ed3d999a872bd4ae76d41b
Michael Murray <53702681+mfmurray@users.noreply.github.com>
2023-12-11 11:47:05 -0500

    fix: enforce dark/light background from appConfig (#43)

--- android/src/main/java/io/rownd/android/util/Constants.kt
+++ android/src/main/java/io/rownd/android/util/Constants.kt
@@ -1,11 +1,13 @@
 package io.rownd.android.util
 
 import android.os.Build
+import androidx.compose.ui.graphics.Color
 import io.rownd.android.BuildConfig
 
 object Constants {
     val DEFAULT_API_USER_AGENT = "Rownd SDK for Android/${BuildConfig.VERSION_NAME} (Language: Kotlin; Platform=Android ${Build.VERSION.BASE_OS} ${Build.VERSION.RELEASE} (SDK ${Build.VERSION.SDK_INT});)"
 
     val DEFAULT_WEB_USER_AGENT = "Rownd SDK for Android/${BuildConfig.VERSION_NAME} (Language: Kotlin; Platform=Android ${Build.VERSION.BASE_OS} ${Build.VERSION.RELEASE} (SDK ${Build.VERSION.SDK_INT});)"
-
+    val BACKGROUND_LIGHT = Color(0xffffffff)
+    val BACKGROUND_DARK = Color(0xff1c1c1e)
 }
